fd59d57a7c732d820873ae404b4b8b4a2fbc4bbc
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-09-03 11:29:22 +0100

    Flexing our Combine skills, add retry and receive to control which thread our pipeline should run on

--- NetworkingWithCombine/ContentView.swift
+++ NetworkingWithCombine/ContentView.swift
@@ -7,12 +7,56 @@
 //
 
 import SwiftUI
+import Combine
+
+struct User: Decodable {
+    var id: UUID
+    var name: String
+    
+    static let `default` = User(id: UUID(), name: "Anonymous")
+}
 
 struct ContentView: View {
+    @State private var requests = Set<AnyCancellable>()
+    
     var body: some View {
-        Text("Hello, World!")
+        Button ("Fetch Data") {
+            let url = URL(string: "https://www.hackingwithswift.com/samples/user-24601.json")!
+            self.fetch(url, defaultValue: User.default) {
+                print($0.name)
+            }
+        }
+    }
+//  func fetch(_ url: URL) {
+//      URLSession.shared.dataTask(with: url) { data, response, error in
+//          if let error = error {
+//              print(User.default.name)
+//          } else if let data = data {
+//              let decoder = JSONDecoder()
+//
+//              do {
+//                  let user = try decoder.decode(User.self, from: data)
+//                  print(user.name)
+//              } catch {
+//                  print(User.default.name)
+//              }
+//          }
+//      }.resume()
+//  }
+    func fetch<T: Decodable>(_ url: URL, defaultValue: T, completion: @escaping (T) -> Void) {
+        let decoder = JSONDecoder()
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+        .retry(1)
+            .map(\.data)
+            .decode(type: T.self, decoder: decoder)
+            .replaceError(with: defaultValue)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: completion)
+            .store(in: &requests)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
